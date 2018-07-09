2feb8693bee9baee34bdc7dc223de4a7e75bf1d1
herton7362 <he.tang@everydayratings.com>
2018-07-09 11:14:30 +0800

    创建一个ant design pro项目

---
+++ .webpackrc.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+export default {
+  entry: 'src/index.js',
+  extraBabelPlugins: [
+    'transform-decorators-legacy',
+    ['import', { libraryName: 'antd', libraryDirectory: 'es', style: true }],
+  ],
+  env: {
+    development: {
+      extraBabelPlugins: ['dva-hmr'],
+      define: {
+        ROOT_PATH: "http://127.0.0.1:8080",
+        NO_PROXY: process.env,
+      },
+    },
+  },
+  alias: {
+    components: path.resolve(__dirname, 'src/components/'),
+  },
+  ignoreMomentLocale: true,
+  theme: './src/theme.js',
+  html: {
+    template: './src/index.ejs',
+  },
+  disableDynamicImport: true,
+  publicPath: '/',
+  hash: true,
+};
